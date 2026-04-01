681183cb89dd990d055dbf397d716dd207e4ec84
lowcode-aidan-20260318 <ha916918@gmail.com>
2026-04-01 09:29:21 +0700

    [AES-0401] add cartproduct

---
+++ Aesthetics.Commons/DTOs/CartProductDetailDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace Aesthetics.Commons.DTOs
+{
+    public class CartProductDetailDTO
+    {
+        public int Id { get; set; }
+        public int? CartId { get; set; }
+        public int? ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal? PriceAtAdd { get; set; }
+        public DateTime? CreateDate { get; set; }
+
+        // Thông tin sản phẩm
+        public string? ProductName { get; set; }
+        public string? ProductImages { get; set; }
+        public string? Description { get; set; }
+        public decimal? SellingPrice { get; set; }
+        public string? Unit { get; set; }
+    }
+}
--- Aesthetics.DataAccess/AestheticsInterfaces/IAccountSessionsService.cs
+++ Aesthetics.DataAccess/AestheticsInterfaces/IAccountSessionsService.cs
@@ -9,14 +9,8 @@ using System.Threading.Tasks;
 
 namespace Aesthetics.Data.AestheticsInterfaces
 {
-    public interface ICartProductService
-    {
-		Task<bool> create(CreateCartProduct request);
-
-		Task<bool> update(UpdateCartProduct request);
-
-		Task<bool> delete(DeleteCartProduct request);
-
-		Task<BaseDataCollection<CartProductResponseModel>> getlist(GetCartProduct request);
+	public interface IAccountSessionsService
+	{
+		Task<BaseDataCollection<AccountSessionEntity>> getlist(getSession session);
 	}
 }
